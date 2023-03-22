5730d09ee81b00554dddf5d4cd2e0a190c625716
chirag-ghosh <cghosh828049@gmail.com>
2023-03-22 19:30:36 +0530

    Init assignment;

---
+++ Asgn6/pingnetinfo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Prints how the program should be run
+void printHelp() {
+    printf("\nPlease use the following format to run PingNetInfo.\n");
+    printf("\n./PingNetInfo <hostname> <n> <T>\n");
+    printf("\nArgument details is given below.\n");
+    printf(" -> hostname: Name of the host like 'cse.iitkgp.ac.in' or an IP address.\n");
+    printf(" -> n: The number of times a probe will be sent per link.\n");
+    printf(" -> T: The time difference between any two probes.\n");
+}
+
+int main(int argc, char** argv) {
+
+    // processing arguments
+    if(argc < 4) printHelp();
+    return 0;
+}
