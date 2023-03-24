71c20d8f79f67f1ad95229fead53a5f13fbb3abf
chirag-ghosh <cghosh828049@gmail.com>
2023-03-24 19:13:34 +0530

    setting ip headers; (wip)

--- Asgn6/pingnetinfo.c
+++ Asgn6/pingnetinfo.c
@@ -5,6 +5,8 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include<netinet/ip.h>
+#include<netinet/ip_icmp.h>
 #include <arpa/inet.h>
 
 // Prints how the program should be run
@@ -15,6 +17,26 @@ void printHelp() {
     printf(" -> hostname: Name of the host like 'cse.iitkgp.ac.in' or an IP address.\n");
     printf(" -> n: The number of times a probe will be sent per link.\n");
     printf(" -> T: The time difference between any two probes (seconds).\n");
+}
+
+// copied from internet. need to change later.
+static unsigned short compute_checksum(unsigned short *addr, unsigned int count) {
+  register unsigned long sum = 0;
+  while (count > 1) {
+    sum += * addr++;
+    count -= 2;
+  }
+  //if any bytes left, pad the bytes and add
+  if(count > 0) {
+    sum += ((*addr)&htons(0xFF00));
+  }
+  //Fold sum to 16 bits: add carrier to result
+  while (sum>>16) {
+      sum = (sum & 0xffff) + (sum >> 16);
+  }
+  //one's complement
+  sum = ~sum;
+  return ((unsigned short)sum);
 }
 
 int main(int argc, char** argv) {
@@ -57,6 +79,47 @@ int main(int argc, char** argv) {
     printf("destination IP Address: %s\n", destIP);
     printf("Number of probes: %d\n", n);
     printf("Time difference between each probe (in seconds): %d\n", T);
+
+    // create raw socket for icmp
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if(sockfd == -1) {
+        perror("Cannot create socket.\n");
+        exit(1);
+    }
+
+    // set socket options
+    int one = 1;
+    if(setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
+        perror("Cannot set socket options.\n");
+        exit(1);
+    }
+
+    // setting up server address and port
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    inet_aton(destIP, &serverAddress.sin_addr);
+    serverAddress.sin_port = htons(7); // 7 is used for echo
+
+    // setup ip header. Copying from internet. Change later.
+    char sendBuf[4096];
+    struct ip *ipHeader = (struct ip *)sendBuf;
+    ipHeader->ip_v = 4;
+    ipHeader->ip_hl = 5;
+    ipHeader->ip_tos = 0;
+    ipHeader->ip_len = 20 + 8;
+    ipHeader->ip_id = 10000;
+    ipHeader->ip_off = 0;
+    ipHeader->ip_p = IPPROTO_ICMP;
+    inet_aton(srcIP, &(ipHeader->ip_src));
+    net_aton(destIP, &(ipHeader->ip_dst));
+
+    // init time-to-live
+    int ttl = 1;
+
+    while(1) {
+        ipHeader->ip_ttl = ttl;
+        ipHeader->ip_sum = compute_checksum((unsigned short *)ipHeader, (ipHeader->ip_hl << 2));
+    }
 
     return 0;
 }
